e0372befdcea2a293f7664f418d7ca0580173fd7
Martin Cifuentes <marcisego@gmail.com>
2022-02-05 20:41:32 -0500

    Comunicación correcta entre screens y add correctamente funcionando, solo falta el log in

--- Pages/Users/Index.cshtml.cs
+++ Pages/Users/Index.cshtml.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using RPUser.Data;
 using RPUser.Models;
 
@@ -20,6 +20,26 @@ namespace RPUser.Pages.Users
             _context = context;
         }
 
-        
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public User User { get; set; }
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            /*if (!ModelState.IsValid)
+            {
+                return Page();
+            }*/
+
+            //_context.User.Add(User);
+            //await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Tableview");
+        }
     }
 }
